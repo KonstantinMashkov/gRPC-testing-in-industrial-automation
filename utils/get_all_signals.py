0b258d6d613bd4ae23cc630885eaf37b55631138
Konstantin <maschkovc@yandex.ru>
2025-12-31 12:54:49 +0300

    add method GetAllSignals

--- utils/get_all_signals.py
+++ utils/get_all_signals.py
@@ -1,11 +1,27 @@
 #   rpc GetAllSignals (Empty) returns (SignalPool) {}
+
+"""
+Модуль для демонстрации запроса к gRPC-серверу с целью получения сигнала.
+Используется протокол буферов Protobuf и библиотека gRPC.
+"""
+
+# Импортируем библиотеку gRPC
 import grpc
+
+# Импортируем сгенерированные файлы
 from proto import elecont_pb2, elecont_pb2_grpc
 
 
-
-
 def get_all_signals(ip_address_and_port):
+    """
+    Осуществляет подключение к gRPC-серверу и запрашивает список всех сигналов.
+
+    Parameters:
+        ip_address_and_port (str): IP-адрес и порт, по которым доступен gRPC-сервер.
+
+    Returns:
+        None: Печать результатов осуществляется в stdout.
+    """
     # Создаем соединение с сервером
     channel = grpc.insecure_channel(ip_address_and_port)
     
@@ -22,8 +38,8 @@ def get_all_signals(ip_address_and_port):
     print(response)
 
 
+# IP-адрес и порт, по которым мы работаем
 ip_user_channel_client = '127.0.0.1:29041'
 
 # Запускаем получение сигналов
 get_all_signals(ip_user_channel_client)
-   
