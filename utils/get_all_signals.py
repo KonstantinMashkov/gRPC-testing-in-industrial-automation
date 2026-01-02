4f5203b8a3a27c3d236338d8973b8705c221146e
Konstantin <maschkovc@yandex.ru>
2026-01-02 05:38:33 +0300

    add try except

--- utils/get_all_signals.py
+++ utils/get_all_signals.py
@@ -18,28 +18,34 @@ def get_all_signals(ip_address_and_port):
 
     Parameters:
         ip_address_and_port (str): IP-адрес и порт, по которым доступен gRPC-сервер.
-
-    Returns:
-        None: Печать результатов осуществляется в stdout.
     """
-    # Создаем соединение с сервером
-    channel = grpc.insecure_channel(ip_address_and_port)
+    try:
+        # Создаем соединение с сервером
+        channel = grpc.insecure_channel(ip_address_and_port)
+        
+        # Создаем прокси-класс для обращения к службе Elecont
+        stub = elecont_pb2_grpc.ElecontStub(channel)
+        
+        # Передаем пустой объект Empty в качестве аргумента
+        empty_request = elecont_pb2.Empty()
+        
+        # Вызываем метод GetAllSignals
+        response = stub.GetAllSignals(empty_request)
+        
+        # Возвращаем полученные данные       
+        return response
     
-    # Создаем прокси-класс для обращения к службе Elecont
-    stub = elecont_pb2_grpc.ElecontStub(channel)
-    
-    # Передаем пустой объект Empty в качестве аргумента
-    empty_request = elecont_pb2.Empty()
-    
-    # Вызываем метод GetAllSignals
-    response = stub.GetAllSignals(empty_request)
-    
-    # Выводим полученный ответ
-    print(response)
+    # Обработка ошибок
+    except grpc.RpcError as e:
+        print(f'gRPC ошибка: {e.details()}')
+            
 
 
 # IP-адрес и порт, по которым мы работаем
 ip_user_channel_client = '127.0.0.1:29041'
 
 # Запускаем получение сигналов
-get_all_signals(ip_user_channel_client)
+response_get_all_signals = get_all_signals(ip_user_channel_client)
+
+# Вывод в консоль полученного результата
+print(response_get_all_signals)
